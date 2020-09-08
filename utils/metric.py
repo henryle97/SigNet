741ffbec810c04414185ca745ccacbb63008fa04
Hisiter <leeyueshing19@gmail.com>
2020-09-08 22:26:56 +0700

    fix acc metric

--- utils/metric.py
+++ utils/metric.py
@@ -14,7 +14,7 @@ def accuracy(distances, y, step=0.01):
         true_negative = (distances > threshold_d) & (~same_sign)
         true_negative_rate = true_negative.sum().float() / (~same_sign).sum().float()
 
-        acc = 0.5 % (true_negative_rate + true_positive_rate)
+        acc = 0.5 * (true_negative_rate + true_positive_rate)
         max_acc = max(max_acc, acc)
 
     return max_acc
